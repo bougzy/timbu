d6e882e19996c8518791c7c4817b0f4fbed741e5
bougzy <yubeespecial@gmail.com>
2024-07-10 08:11:01 +0100

    added new feature

--- src/Components/Home/Arrival.jsx
+++ src/Components/Home/Arrival.jsx
@@ -3,7 +3,9 @@ import { Container, Row, Col, Image } from 'react-bootstrap';
 import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
 import './Arrival.css'; 
 import arrowleft from "../../assets/arrowleft.png"
-import arrowright from "../../assets/arrowright.png"
+import fireleft from "../../assets/fireleft.png"
+// import arrowright from "../../assets/arrowright.png"
+import fireright from "../../assets/fireright.png"
 
 const TwoColumnGrid = () => {
   return (
@@ -15,11 +17,11 @@ const TwoColumnGrid = () => {
         <Col className="d-flex justify-content-end align-items-center me-1">
           <div className="circle-icon">
             
-            <Image src={arrowleft} className="me-5"/>
+            <Image src={fireleft} className="me-5"/>
           </div>
           <div className="circle-icon">
            
-          <Image src={arrowright} className="" />
+          <Image src={fireright} className="" />
           </div>
         </Col>
       </Row>
